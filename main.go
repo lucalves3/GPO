b888b7901661c30cd23c889737a24abc94545150
Francisco Lucas Oliveira Alves <francisco@radek.com.br>
2022-12-18 19:35:37 -0300

    :rocket: test
    test

--- main.go
+++ main.go
@@ -2,17 +2,21 @@ package gpo
 
 import "fmt"
 
-func Message(name string) string {
+type Resultado struct {
+	result string
+}
+
+func (r *Resultado) Message(name string) string {
 	result := fmt.Sprintf("Have a nice day %s", name)
 	return result
 }
 
-func TestMessage(name string) string {
+func (r *Resultado) TestMessage(name string) string {
 	result := fmt.Sprintf("Have a nice night %s", name)
 	return result
 }
 
-func TestNewMessage(name string) string {
+func (r *Resultado) TestNewMessage(name string) string {
 	result := fmt.Sprintf("Have a nice week %s", name)
 	return result
 }
