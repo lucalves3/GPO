d45ab0e4256f33fa975fcfefb5fef560ce4290c1
Francisco Lucas Oliveira Alves <francisco@radek.com.br>
2022-12-18 18:11:47 -0300

    :rocket: test my package
    test my package

--- main.go
+++ main.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world!!")
+	CreateAMessage("Lucas")
+}
+
+func CreateAMessage(name string) string {
+	result := fmt.Sprintf("Have a nice day %s", name)
+	return result
 }
