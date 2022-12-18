9222a4b9e597a4dda54a2cd59520d6efe69b2c6d
Francisco Lucas Oliveira Alves <francisco@radek.com.br>
2022-12-18 18:31:20 -0300

    :rocket: test the package
    test the package

--- gpo.go
+++ gpo.go
@@ -6,3 +6,13 @@ func Message(name string) string {
 	result := fmt.Sprintf("Have a nice day %s", name)
 	return result
 }
+
+func TestMessage(name string) string {
+	result := fmt.Sprintf("Have a nice night %s", name)
+	return result
+}
+
+func TestNewMessage(name string) string {
+	result := fmt.Sprintf("Have a nice week %s", name)
+	return result
+}
