c8fb3ee9174b7590dff1b9dee47c735b85434209
Francisco Lucas Oliveira Alves <francisco@radek.com.br>
2022-12-19 14:54:13 -0300

    :rocket: modify the gpo program
    modify the gpo program

--- gpo.go
+++ gpo.go
@@ -1,16 +1,66 @@
-package main
+package gpo
 
 import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
-type Gpo struct {
-	*Gpo
+type CreateStructure struct {
+	err error
 }
 
-func (gpo *Gpo) CreateModelExampleFile(projectName string, modelName string) {
+func createSRCFolder() error {
+	path, _ := os.Getwd()
+	localFile := fmt.Sprintf("%s/%s", path, "src")
+	err := os.Mkdir(localFile, 0755)
+	return err
+}
+
+func createModelsFolder() error {
+	path, _ := os.Getwd()
+	localFile := fmt.Sprintf("%s/%s", path, "src/models")
+	err := os.Mkdir(localFile, 0755)
+	return err
+}
+
+func createControllersFolder() error {
+	path, _ := os.Getwd()
+	localFile := fmt.Sprintf("%s/%s", path, "src/controllers")
+	err := os.Mkdir(localFile, 0755)
+	return err
+}
+
+func createEntitiesFolder() error {
+	path, _ := os.Getwd()
+	localFile := fmt.Sprintf("%s/%s", path, "src/entities")
+	err := os.Mkdir(localFile, 0755)
+	return err
+}
+
+func createRoutesFolder() error {
+	path, _ := os.Getwd()
+	localFile := fmt.Sprintf("%s/%s", path, "src/routes")
+	err := os.Mkdir(localFile, 0755)
+	return err
+}
+
+func (c *CreateStructure) CreateAllProjectStructureFolders() error {
+	createSRCFolder()
+	time.Sleep(1 * time.Second)
+	createModelsFolder()
+	time.Sleep(1 * time.Second)
+	createControllersFolder()
+	time.Sleep(1 * time.Second)
+	createEntitiesFolder()
+	time.Sleep(1 * time.Second)
+	err := createRoutesFolder()
+	time.Sleep(1 * time.Second)
+	return err
+}
+
+func CreateModelExampleFile(projectName string, modelName string) {
 	path, _ := os.Getwd()
 	localFile := fmt.Sprintf("%s/%s", path, "src/models")
 	firstCapitalCase := strings.Title(modelName)
