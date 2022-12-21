1a41714fc246c59394be98ca9e14622566456807
Francisco Lucas Oliveira Alves <francisco@radek.com.br>
2022-12-21 03:09:25 -0300

    :sparkles: model file att
    model file att

--- modelExample.go
+++ modelExample.go
@@ -97,10 +97,44 @@ func CheckIfIndexModelWasCreated(projectName string) {
 		fileText := fmt.Sprintf(`package models
 
 		import (
+			"%s/entities"
+			"fmt"
+			"os"
+		
+			"github.com/joho/godotenv"
+			"gorm.io/driver/mysql"
 			"gorm.io/gorm"
 		)
+
+		var db *gorm.DB
 		
-		var db *gorm.DB`)
+		func ConnectDB() (*gorm.DB, error) {
+			err := godotenv.Load(".env")
+			if err != nil {
+				fmt.Println("[ERROR]: Error get env")
+			}
+			dsn := fmt.Sprint(os.Getenv("DBUSER"), ":", os.Getenv("DBPASSWORD"), "@tcp(", os.Getenv("DBIP"), ":", os.Getenv("DBPORT"), ")/", os.Getenv("DBSCHEMA"), "?charset=utf8mb4&parseTime=True&loc=Local")
+			fmt.Println(dsn)
+			db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		
+			if err != nil {
+				fmt.Println("[ERROR]: Error creating database")
+			}
+			ConfigDB(db)
+			return db, err
+		}
+		
+		func ConfigDB(db *gorm.DB) {
+		
+			db.AutoMigrate(&entities.CHANGEHERE{})
+		}
+		
+		func CreateConnectionDB() error {
+			var err error
+			db, err = ConnectDB()
+			return err
+		}
+		`, projectName)
 
 		_, err = file.WriteString(fileText)
 		if err != nil {
